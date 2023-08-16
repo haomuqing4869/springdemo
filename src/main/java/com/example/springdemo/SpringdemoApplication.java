8b0c9f051948636b8b44ef760e4cd79f83391d28
haomuqing4869 <1450470243@qq.com>
2023-08-16 18:57:39 +0800

    第四版本，删除多余文件

--- src/main/java/com/example/springdemo/SpringdemoApplication.java
+++ src/main/java/com/example/springdemo/SpringdemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan(basePackages = "com.example.springdemo.mybatis.dao")
 public class SpringdemoApplication {
     //http://127.0.0.1:8088/springdemo/service/getFoodInfo
-    //http://127.0.0.1:8088/springdemo/food
+    //http://127.0.0.1:8088/springdemo/food/get/1
     public static void main(String[] args) {
         SpringApplication.run(SpringdemoApplication.class, args);
     }
